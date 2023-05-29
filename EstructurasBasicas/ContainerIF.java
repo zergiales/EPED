3f76d65c69bce165e3f09713b35ce7b75dbbeb73
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-05-29 14:09:16 +0200

    ContainerIf<E>

--- EstructurasBasicas/ContainerIF.java
+++ EstructurasBasicas/ContainerIF.java
@@ -2,11 +2,11 @@ package EstructurasBasicas;
 /*
  * Caso donde nos centramos en el funcionamiento de un contendor
  * Un contenedor es una coleccion de elementos que NO GUARDAN 
- * ningún tipo de orden entre si.
+ * ningÃºn tipo de orden entre si.
  */
 public interface ContainerIF<E> extends CollectionIF<E> {
-	//añade un elemento e al contenedor
-	public void add(E e);
+	//aÃ±ade un elemento e al contenedor
+	public void add(E e); 
 	
 	/*
 	 * Elimina el elemnto e del contenedor
@@ -17,11 +17,12 @@ public interface ContainerIF<E> extends CollectionIF<E> {
 	 * Devuelve un valor que indica si una subcadena especificada
 	 * aparece dentro de esta cadena.
 	 * 
-	 * @post: !this.contains(e)
+	 * @post: !this.contains(e) --> luego no esta
 	 * NOTA: Si ponemos ! es para hacer una negacion de la misma
 	 */
 	public void remove(E e);
 	
-	//devuelve un iterador para el contenedor
+	//devuelve un iterador e para el contenedor
+	/** el como los reocrra dependera d ela implemntacion complementa del contendor*/
 	public IteradorIf<E> iterador();
 }
