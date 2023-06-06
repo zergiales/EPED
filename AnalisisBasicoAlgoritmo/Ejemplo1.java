0fb21e4c76e529f4bcbb317280e5c99602e50e5c
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-06-06 19:48:38 +0200

    operaciones basicas

--- AnalisisBasicoAlgoritmo/Ejemplo1.java
+++ AnalisisBasicoAlgoritmo/Ejemplo1.java
@@ -7,14 +7,21 @@ public class Ejemplo1 {
      * 
      * operación basica de coste de cte 0 -> 1
      * @param v[] que es el que vamos a ordenar , el arreglo v
+     * 
+     * 1. Definimos cual es el tamaño del problema 
+     * 2. Calculamos el coste de cada una de las instrucciones y luego irlo acumulando
+     * 
      */
     public void ordenar( int[] v){
+        //coste del for
         for(int i =2; i<= v.length;i++){
             //campos variables 
             int p= i;
             int x =v[i];
             boolean seguir = true;
+            //coste del while
             while (p > 1 && seguir) {
+                //coste del if 
                 if (x< v[p - 1]) {
                     v[p] = v[p - 1];
                     p =p-1;
---
+++ AnalisisBasicoAlgoritmo/README.md
@@ -0,0 +1,7 @@
+# Operaciones basicas
+ * No depende del problema
+    - Operaciones de entrada / salida
+    - Asignacion
+    - Expresiones
+* Coste constante : 0(1)
+-----
